09dfdec039fda37574fc1c3d4922e92839270052
Jens Colman <colmanjens@gmail.com>
2019-03-27 17:56:50 +0100

    Service worker

--- scripts/serviceWorker.js
+++ scripts/serviceWorker.js
@@ -1,45 +1,18 @@
-// // Cache version
-// var cacheName = "v1";
-// // Aanduiden welke files er gecached moeten worden.
-// var cacheFiles = [
-//      "../scripts/",
-//      "../scripts/index.js",
-//      "../scripts/timer.js",
-//      "../styles/index.scss",
-//      "../index.html",
-//      "../challenges.html",
-//      "../extraIdeas.html",
-//      "../usePoints.html",
-//      "../videoResources.html",
-//      "https://fonts.googleapis.com/css?family=Noto+Sans+KR:100,400,700"
-// ];
-
-// Installeren van serviceworker.
-self.addEventListener("install", function(e) {
-     console.log("[serviceWorker] installed");
-     // De installatie zal moeten wachten tot dit is uitgevoerd.
-     // e.waitUntil(
-     //      caches.open(cacheName).then(function(cache) {
-     //           console.log("[serviceWorker] caching cacheFiles");
-     //           return cache.addAll(cacheFiles);
-     //      })
-     // );
-});
-
-// Activeren van serviceworker.
-self.addEventListener("activate", function(e) {
-     console.log("[serviceWorker] activated");
-});
-
-// Fetching van serviceworker.
-self.addEventListener("fetch", function(e) {
-     console.log("[serviceWorker] fetching ", e.request.url);
-});
-
-
-
-
-
+// Cache version
+var cacheName = "v1";
+// Aanduiden welke files er gecached moeten worden.
+var cacheFiles = [
+     "../scripts/",
+     "../scripts/index.js",
+     "../scripts/timer.js",
+     "../styles/index.scss",
+     "../index.html",
+     "../challenges.html",
+     "../extraIdeas.html",
+     "../usePoints.html",
+     "../videoResources.html",
+     "https://fonts.googleapis.com/css?family=Noto+Sans+KR:100,400,700"
+];
 
 //// Use a namespace to prevent pollution of the global namespace.
 //var rli = rli || {};
@@ -95,3 +68,25 @@ self.addEventListener("fetch", function(e) {
 //http_request.withCredentials = true;
 //http_request.setRequestHeader("Content-Type", "application/json");
 //http_request.send({ 'request': "authentication token" });
+
+// Installeren van serviceworker.
+self.addEventListener("install", function(e) {
+     console.log("[serviceWorker] installed");
+     // De installatie zal moeten wachten tot dit is uitgevoerd.
+     e.waitUntil(
+          caches.open(cacheName).then(function(cache) {
+               console.log("[serviceWorker] caching cacheFiles");
+               return cache.addAll(cacheFiles);
+          })
+     );
+});
+
+// Activeren van serviceworker.
+self.addEventListener("activate", function(e) {
+     console.log("[serviceWorker] activated");
+});
+
+// Fetching van serviceworker.
+self.addEventListener("fetch", function(e) {
+     console.log("[serviceWorker] fetching ", e.request.url);
+});
