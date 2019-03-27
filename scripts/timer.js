b80189d499d651f652ea00ca2e19c5cc83d6583d
Jens Colman <colmanjens@gmail.com>
2019-03-28 00:41:35 +0100

    Update timer.js

--- scripts/timer.js
+++ scripts/timer.js
@@ -405,29 +405,29 @@ if ("serviceWorker" in navigator) {
 
 // ---------------------- HTTP request ---------------------- \\
 
-// // Function to perform HTTP request
-// var get = function(url) {
-//   return new Promise(function(resolve, reject) {
-//
-//     var xhr = new XMLHttpRequest();
-//     xhr.onreadystatechange = function() {
-//         if (xhr.readyState === XMLHttpRequest.DONE) {
-//             if (xhr.status === 200) {
-//                 var result = xhr.responseText;
-//                 result = JSON.parse(result);
-//                 resolve(result);
-//             } else {
-//                 reject(xhr);
-//             }
-//         }
-//     };
-//
-//     xhr.open("GET", url, true);
-//     xhr.send();
-//
-//   });
-// };
-//
+// Function to perform HTTP request
+var get = function(url) {
+  return new Promise(function(resolve, reject) {
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                var result = xhr.responseText;
+                result = JSON.parse(result);
+                resolve(result);
+            } else {
+                reject(xhr);
+            }
+        }
+    };
+
+    xhr.open("GET", url, true);
+    xhr.send();
+
+  });
+};
+
 // // Hier kan je de externe link opvragen.
 // get('https://api.nasa.gov/planetary/earth/imagery?api_key=fWfSMcDzyHfMuH3BW6jiIUBYaj3hKRyKBRTBqgEQ')
 //   .then(function(response) {
