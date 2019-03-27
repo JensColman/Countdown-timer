2314d24db2777a60bca24fbddf70470977f17006
Jens Colman <colmanjens@gmail.com>
2019-03-28 00:11:45 +0100

    Update service-worker.js

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -21,41 +21,6 @@ var cacheFiles = [
 	"https://fonts.googleapis.com/css?family=Noto+Sans+KR:100,400,700"
 ];
 
-
-// Installeren van serviceworker.
-self.addEventListener("install", function(e) {
-	console.log("[serviceWorker] Installed");
-	// De installatie zal moeten wachten tot dit is uitgevoerd.
-	e.waitUntil(
-		caches.open(cacheName).then(function(cache) {
-			console.log("[serviceWorker] Caching cacheFiles " + cacheName);
-               // Hier wordt alle files die opgegeven zijn in cacheFiles opgeslagen in de cache.
-			return cache.addAll(cacheFiles);
-		})
-	);
-});
-
-// Activeren van serviceworker.
-self.addEventListener("activate", function(e) {
-	console.log("[serviceWorker] Activated");
-
-     // Dit zorgt ervoor dat files van oude caches niet bewaard blijven.
-     e.waitUntil(
-          // Hier worden alle keys en cache overlopen.
-          caches.keys().then(function(cacheNames) {
-               // Hier wordt er gelooped door de cacheNames om te zien of er geen verouderde bestanden bijzitten.
-               return Promise.all(cacheNames.map(function(thisCacheName) {
-                    // Hier wordt nagekeken of er versies zijn die verschillen van de huidige cacheName.
-                    if (thisCacheName !== cacheName) {
-                         console.log("[serviceWorker] Removing cached files from ", thisCacheName);
-                         // Hier worden de caches verwijderd die niet overeenkomen met de huidige cacheName.
-                         return caches.delete(thisCacheName);
-                    }
-               }));
-          })
-     );
-});
-
 // Fetching van serviceworker.
 self.addEventListener("fetch", function(e) {
 	console.log("[serviceWorker] Fetching ", e.request.url);
@@ -87,6 +52,40 @@ self.addEventListener("fetch", function(e) {
                }).catch(function(err) {
                     console.log("[serviceWorker] Error fetching & caching new data", err);
                });
+          })
+     );
+});
+
+// Installeren van serviceworker.
+self.addEventListener("install", function(e) {
+	console.log("[serviceWorker] Installed");
+	// De installatie zal moeten wachten tot dit is uitgevoerd.
+	e.waitUntil(
+		caches.open(cacheName).then(function(cache) {
+			console.log("[serviceWorker] Caching cacheFiles " + cacheName);
+               // Hier wordt alle files die opgegeven zijn in cacheFiles opgeslagen in de cache.
+			return cache.addAll(cacheFiles);
+		})
+	);
+});
+
+// Activeren van serviceworker.
+self.addEventListener("activate", function(e) {
+	console.log("[serviceWorker] Activated");
+
+     // Dit zorgt ervoor dat files van oude caches niet bewaard blijven.
+     e.waitUntil(
+          // Hier worden alle keys en cache overlopen.
+          caches.keys().then(function(cacheNames) {
+               // Hier wordt er gelooped door de cacheNames om te zien of er geen verouderde bestanden bijzitten.
+               return Promise.all(cacheNames.map(function(thisCacheName) {
+                    // Hier wordt nagekeken of er versies zijn die verschillen van de huidige cacheName.
+                    if (thisCacheName !== cacheName) {
+                         console.log("[serviceWorker] Removing cached files from ", thisCacheName);
+                         // Hier worden de caches verwijderd die niet overeenkomen met de huidige cacheName.
+                         return caches.delete(thisCacheName);
+                    }
+               }));
           })
      );
 });
