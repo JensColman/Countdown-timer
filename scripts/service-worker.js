a502a315dc27affb86e096049c027af02bc7b7c5
Jens Colman <colmanjens@gmail.com>
2019-03-28 00:27:47 +0100

    Update service-worker.js

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -20,37 +20,75 @@ var cacheFiles = [
 ];
 
 // Installeren van serviceworker.
-self.addEventListener("install", function(e) {
-	console.log("[serviceWorker] Installed");
-	// De installatie zal moeten wachten tot dit is uitgevoerd.
-	e.waitUntil(
-		caches.open(cacheName).then(function(cache) {
-			console.log("[serviceWorker] Caching cacheFiles " + cacheName);
-               // Hier wordt alle files die opgegeven zijn in cacheFiles opgeslagen in de cache.
+// self.addEventListener("install", function(e) {
+// 	console.log("[serviceWorker] Installed");
+// 	// De installatie zal moeten wachten tot dit is uitgevoerd.
+// 	e.waitUntil(
+// 		caches.open(cacheName).then(function(cache) {
+// 			console.log("[serviceWorker] Caching cacheFiles " + cacheName);
+//                // Hier wordt alle files die opgegeven zijn in cacheFiles opgeslagen in de cache.
+// 			return cache.addAll(cacheFiles);
+// 		})
+// 	);
+// });
+
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Installed');
+
+    // e.waitUntil Delays the event until the Promise is resolved
+    e.waitUntil(
+
+    	// Open the cache
+	    caches.open(cacheName).then(function(cache) {
+
+	    	// Add all the default files to the cache
+			console.log('[ServiceWorker] Caching cacheFiles');
 			return cache.addAll(cacheFiles);
-		})
-	);
+	    })
+	); // end e.waitUntil
 });
 
 // Activeren van serviceworker.
-self.addEventListener("activate", function(e) {
-	console.log("[serviceWorker] Activated");
+// self.addEventListener("activate", function(e) {
+// 	console.log("[serviceWorker] Activated");
+//
+//      // Dit zorgt ervoor dat files van oude caches niet bewaard blijven.
+//      e.waitUntil(
+//           // Hier worden alle keys en cache overlopen.
+//           caches.keys().then(function(cacheNames) {
+//                // Hier wordt er gelooped door de cacheNames om te zien of er geen verouderde bestanden bijzitten.
+//                return Promise.all(cacheNames.map(function(thisCacheName) {
+//                     // Hier wordt nagekeken of er versies zijn die verschillen van de huidige cacheName.
+//                     if (thisCacheName !== cacheName) {
+//                          console.log("[serviceWorker] Removing cached files from ", thisCacheName);
+//                          // Hier worden de caches verwijderd die niet overeenkomen met de huidige cacheName.
+//                          return caches.delete(thisCacheName);
+//                     }
+//                }));
+//           })
+//      );
+// });
 
-     // Dit zorgt ervoor dat files van oude caches niet bewaard blijven.
-     e.waitUntil(
-          // Hier worden alle keys en cache overlopen.
-          caches.keys().then(function(cacheNames) {
-               // Hier wordt er gelooped door de cacheNames om te zien of er geen verouderde bestanden bijzitten.
-               return Promise.all(cacheNames.map(function(thisCacheName) {
-                    // Hier wordt nagekeken of er versies zijn die verschillen van de huidige cacheName.
-                    if (thisCacheName !== cacheName) {
-                         console.log("[serviceWorker] Removing cached files from ", thisCacheName);
-                         // Hier worden de caches verwijderd die niet overeenkomen met de huidige cacheName.
-                         return caches.delete(thisCacheName);
-                    }
-               }));
-          })
-     );
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activated');
+
+    e.waitUntil(
+
+    	// Get all the cache keys (cacheName)
+		caches.keys().then(function(cacheNames) {
+			return Promise.all(cacheNames.map(function(thisCacheName) {
+
+				// If a cached item is saved under a previous cacheName
+				if (thisCacheName !== cacheName) {
+
+					// Delete that cached file
+					console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
+					return caches.delete(thisCacheName);
+				}
+			}));
+		})
+	); // end e.waitUntil
+
 });
 
 // Fetching van serviceworker.
