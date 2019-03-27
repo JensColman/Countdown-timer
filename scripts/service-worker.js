7a46ae3b44f56d624cb2920b591aa73ec4597c92
Jens Colman <colmanjens@gmail.com>
2019-03-27 21:03:40 +0100

    Update service-worker.js

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,30 +1,32 @@
-// // Cache version
-// var cacheName = "v1";
-// // Aanduiden welke files er gecached moeten worden.
-// var cacheFiles = [
-// 	"../scripts/",
-// 	"../scripts/index.js",
-// 	"../scripts/timer.js",
-// 	"../styles/index.scss",
-// 	"../index.html",
-// 	"../challenges.html",
-// 	"../extraIdeas.html",
-// 	"../usePoints.html",
-// 	"../videoResources.html",
-// 	"https://fonts.googleapis.com/css?family=Noto+Sans+KR:100,400,700"
-// ];
+// Cache version
+var cacheName = "v1";
+// Aanduiden welke files er gecached moeten worden.
+var cacheFiles = [
+     "/Countdown-timer/",
+     "/Countdown-timer/index.html",
+	"/Countdown-timer/challenges.html",
+	"/Countdown-timer/extraIdeas.html",
+	"/Countdown-timer/usePoints.html",
+	"/Countdown-timer/videoResources.html",
+	"/Countdown-timer/scripts/",
+	"/Countdown-timer/scripts/index.js",
+	"/Countdown-timer/scripts/timer.js",
+     "/Countdown-timer/styles/",
+	"/Countdown-timer/styles/index.scss",
+	"https://fonts.googleapis.com/css?family=Noto+Sans+KR:100,400,700"
+];
 
 
 // Installeren van serviceworker.
 self.addEventListener("install", function(e) {
 	console.log("[serviceWorker] installed");
 	// De installatie zal moeten wachten tot dit is uitgevoerd.
-	// e.waitUntil(
-	// 	caches.open(cacheName).then(function(cache) {
-	// 		console.log("[serviceWorker] caching cacheFiles");
-	// 		return cache.addAll(cacheFiles);
-	// 	})
-	// );
+	e.waitUntil(
+		caches.open(cacheName).then(function(cache) {
+			console.log("[serviceWorker] caching cacheFiles");
+			return cache.addAll(cacheFiles);
+		})
+	);
 });
 
 // Activeren van serviceworker.
