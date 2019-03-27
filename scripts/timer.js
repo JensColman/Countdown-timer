5bb874372c6058517556a5dd7f6119988dcdfddc
Jens Colman <colmanjens@gmail.com>
2019-03-27 19:02:23 +0100

    Update timer.js

--- scripts/timer.js
+++ scripts/timer.js
@@ -407,7 +407,7 @@ calculateTime.addEventListener("click", function() {
 console.log("%c %s", consoleCss2, "ServiceWorker: ");
 if ("serviceWorker" in navigator) {
 	navigator.serviceWorker
-		.register("serviceWorker.js", {scope: "/"})
+		.register("/serviceWorker.js", {scope: "/"})
 		.then(function(registration) {
 		console.log("Serviceworker registered. ", registration);
 	})
