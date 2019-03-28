fa14627c7a4e825c843acfdd29b31dfd43b6084d
Jens Colman <colmanjens@gmail.com>
2019-03-28 23:22:00 +0100

    Sidebar

--- scripts/index.js
+++ scripts/index.js
@@ -19,52 +19,95 @@ function emptySidebarTimeSetting() {
      }
 }
 
-if (window.localStorage.getItem("timerSettingDays") >= 1) {
-     importSidebarContent();
-     if (document.getElementById("date-hour")) {
-          if (window.localStorage.getItem("timerSettingDays") == 1) {
-               document.getElementById("date-hour").innerHTML = " day";
-          } else {
-               document.getElementById("date-hour").innerHTML = " days";
-          }
-     }
-     console.log("Days");
+// Berekend het enkelvoud of meervoud om te plaatsen in de HTML.
+function showTimeSettingSidebar() {
 
-} else if (window.localStorage.getItem("timerSettingDays") <= 0 && window.localStorage.getItem("timerSettingHours") >= 1) {
-     importSidebarContent();
-     if (document.getElementById("date-hour")) {
-          if (window.localStorage.getItem("timerSettingHours") == 1) {
-               document.getElementById("date-hour").innerHTML = " hour";
-          } else {
-               document.getElementById("date-hour").innerHTML = " hours";
+     if (window.localStorage.getItem("timerSettingDays") >= 1) {
+          // Plaatst een teller sidebar in de pagina die nog opgevuld moet worden met de resterende tijd.
+          importSidebarContent();
+          if (document.getElementById("date-hour")) {
+               if (window.localStorage.getItem("timerSettingDays") == 1) {
+                    document.getElementById("date-hour").innerHTML = window.localStorage.getItem("timerSettingDays") + " day";
+               } else {
+                    document.getElementById("date-hour").innerHTML = window.localStorage.getItem("timerSettingDays") + " days";
+               }
           }
-     }
-     console.log("Hours");
+          console.log(window.localStorage.getItem("timerSettingDays"));
 
-} else if (window.localStorage.getItem("timerSettingHours") <= 0 && window.localStorage.getItem("timerSettingMinutes") >= 1) {
-     importSidebarContent();
-     if (document.getElementById("date-hour")) {
-          if (window.localStorage.getItem("timerSettingMinutes") == 1) {
-               document.getElementById("date-hour").innerHTML = " minute";
-          } else {
-               document.getElementById("date-hour").innerHTML = " minutes";
+     } else if (window.localStorage.getItem("timerSettingDays") <= 0 && window.localStorage.getItem("timerSettingHours") >= 1) {
+          // Plaatst een teller sidebar in de pagina die nog opgevuld moet worden met de resterende tijd.
+          importSidebarContent();
+          if (document.getElementById("date-hour")) {
+               if (window.localStorage.getItem("timerSettingHours") == 1) {
+                    document.getElementById("date-hour").innerHTML = window.localStorage.getItem("timerSettingHours") + " hour";
+               } else {
+                    document.getElementById("date-hour").innerHTML = window.localStorage.getItem("timerSettingHours") + " hours";
+               }
           }
-     }
-     console.log("Minutes");
+          console.log(window.localStorage.getItem("timerSettingHours"));
 
-} else if (window.localStorage.getItem("timerSettingMinutes") <= 0 && window.localStorage.getItem("timerSettingSeconds") >= 1) {
-     importSidebarContent();
-     if (document.getElementById("date-hour")) {
-          if (window.localStorage.getItem("timerSettingSeconds") == 1) {
-               document.getElementById("date-hour").innerHTML = " second";
-          } else {
-               document.getElementById("date-hour").innerHTML = " seconds";
+     } else if (window.localStorage.getItem("timerSettingHours") <= 0 && window.localStorage.getItem("timerSettingMinutes") >= 1) {
+          // Plaatst een teller sidebar in de pagina die nog opgevuld moet worden met de resterende tijd.
+          importSidebarContent();
+          if (document.getElementById("date-hour")) {
+               if (window.localStorage.getItem("timerSettingMinutes") == 1) {
+                    document.getElementById("date-hour").innerHTML = window.localStorage.getItem("timerSettingMinutes") + " minute";
+               } else {
+                    document.getElementById("date-hour").innerHTML = window.localStorage.getItem("timerSettingMinutes") + " minutes";
+               }
           }
-     }
-     console.log("Seconds");
+          console.log(window.localStorage.getItem("timerSettingMinutes"));
 
-} else {
-     importSidebarContent();
-     emptySidebarTimeSetting();
-     console.log("Niets");
+     } else if (window.localStorage.getItem("timerSettingMinutes") <= 0 && window.localStorage.getItem("timerSettingSeconds") >= 1) {
+          // Plaatst een teller sidebar in de pagina die nog opgevuld moet worden met de resterende tijd.
+          importSidebarContent();
+          if (document.getElementById("date-hour")) {
+               if (window.localStorage.getItem("timerSettingSeconds") == 1) {
+                    document.getElementById("date-hour").innerHTML = window.localStorage.getItem("timerSettingSeconds") +  " second";
+               } else {
+                    document.getElementById("date-hour").innerHTML = window.localStorage.getItem("timerSettingSeconds") + " seconds";
+               }
+          }
+          console.log(window.localStorage.getItem("timerSettingSeconds"));
+
+     } else {
+          // Plaatst een teller sidebar in de pagina die nog opgevuld moet worden met de resterende tijd.
+          importSidebarContent();
+          // Zet de timer placeholder op 0.
+          emptySidebarTimeSetting();
+          // Reset de timer.
+          clearInterval(timer2);
+          // Wist de localstorage.
+          localStorage.removeItem("timerSetting");
+          localStorage.removeItem("timerSettingDays");
+          localStorage.removeItem("timerSettingHours");
+          localStorage.removeItem("timerSettingMinutes");
+          localStorage.removeItem("timerSettingSeconds");
+          // Geeft een notificatie wanneer de timer gestopt is.
+          notificationTimerEnd();
+          console.log("Niets");
+     }
 }
+
+var timer2 = setInterval(function() {
+     // Resterende tijd berekenen en opslaan in localstorage.
+     calculateRestingTime();
+     // Berekend het enkelvoud of meervoud om te plaatsen in de HTML.
+     showTimeSettingSidebar();
+}, 1000);
+
+// // Controleert in de localstorage of er een datum is ingesteld in "timersetting".
+// if (localStorage.getItem("timerSetting") === null) {
+//      // Zet de timer placeholder op 0.
+//      emptySidebarTimeSetting();
+// } else {
+//      // Plaatst een teller sidebar in de pagina die nog opgevuld moet worden met de resterende tijd.
+//      importSidebarContent();
+//
+//      var timer2 = setInterval(function() {
+//           // Resterende tijd berekenen en opslaan in localstorage.
+//           calculateRestingTime();
+//           // Berekend het enkelvoud of meervoud om te plaatsen in de HTML.
+//           showTimeSettingSidebar();
+//      }, 1000);
+// }
