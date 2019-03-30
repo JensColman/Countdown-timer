ada2b39a07f57c1d454e54b15f50abf1c19ed3eb
Jens Colman <colmanjens@gmail.com>
2019-03-31 00:06:10 +0100

    Firebase

--- scripts/cacheSaver.js
+++ scripts/cacheSaver.js
@@ -12,17 +12,7 @@ if ("serviceWorker" in navigator) {
           })
           .then(function(registration) {
                console.log("[serviceWorker] Registered. ");
-
-
-
-
-
-               // Nog na te kijken of dit correct geplaatst is.
-               messaging.useServiceWorker(registration);
-
-
-
-
+               firebase.messaging().useServiceWorker(registration);
           })
           .catch(function(err) {
                console.log("[serviceWorker] Failed to register. ", err);
