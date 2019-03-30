03e79721cbbd7a184da389f2619648a70e486859
Jens Colman <colmanjens@gmail.com>
2019-03-30 17:36:11 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,6 +3,15 @@
 importScripts("https://www.gstatic.com/firebasejs/5.9.2/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/5.9.2/firebase-messaging.js");
 
+if ('serviceWorker' in navigator) {
+     navigator.serviceWorker.register('/Countdown-timer/service-worker.js')
+          .then(function(registration) {
+               console.log('Registration successful, scope is:', registration.scope);
+          }).catch(function(err) {
+               console.log('Service worker registration failed, error:', err);
+          });
+}
+
 // Initialize Firebase.
 var config = {
      apiKey: "AIzaSyDpyti5var4iXdnKG_EIpAZgTKQRnjFLps",
