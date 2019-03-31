8e0397c30b9c1fafa065aecb8624db1925e2360f
Jens Colman <colmanjens@gmail.com>
2019-03-31 07:51:18 +0200

    Update notifications.js

--- scripts/notifications.js
+++ scripts/notifications.js
@@ -36,20 +36,20 @@ if ("serviceWorker" in navigator) {
                          messaging.getToken()
                               .then(function(currentToken) {
                                    if (currentToken) {
-                                        sendTokenToServer(currentToken);
-                                        updateUIForPushEnabled(currentToken);
+                                        messaging.sendTokenToServer(currentToken);
+                                        messaging.updateUIForPushEnabled(currentToken);
                                    } else {
                                         // Show permission request.
                                         console.log('No Instance ID token available. Request permission to generate one.');
                                         // Show permission UI.
-                                        updateUIForPushPermissionRequired();
-                                        setTokenSentToServer(false);
+                                        messaging.updateUIForPushPermissionRequired();
+                                        messaging.setTokenSentToServer(false);
                                    }
                               })
                               .catch(function(err) {
                                    console.log('An error occurred while retrieving token. ', err);
-                                   showToken('Error retrieving Instance ID token. ', err);
-                                   setTokenSentToServer(false);
+                                   messaging.showToken('Error retrieving Instance ID token. ', err);
+                                   messaging.setTokenSentToServer(false);
                               });
 
                          return messaging.getToken();
