3102dcc21eff5c4995db72a2bef05ec722ad6f65
Jens Colman <colmanjens@gmail.com>
2019-03-31 05:23:50 +0200

    Firebase

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,6 @@
 
 // ---------------------- Firebase  ---------------------- \\
 
-importScripts("https://www.gstatic.com/firebasejs/5.9.2/firebase.js");
 importScripts("https://www.gstatic.com/firebasejs/5.9.2/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/5.9.2/firebase-messaging.js");
 
